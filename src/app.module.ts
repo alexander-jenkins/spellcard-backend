54876200b521690d3725514a987169a2b4e0309a
Alexander Jenkins <22840490+alexander-jenkins@users.noreply.github.com>
2022-06-08 18:15:13 -0400

    added spell and user stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { SpellModule } from './spell/spell.module';
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forRoot()],
+  imports: [AuthModule, TypeOrmModule.forRoot(), UserModule, SpellModule],
   controllers: [AppController],
   providers: [AppService],
 })
