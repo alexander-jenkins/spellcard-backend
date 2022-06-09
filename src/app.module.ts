b3f3c5749025a88915d48d8dec2e995edd677178
Alexander Jenkins <22840490+alexander-jenkins@users.noreply.github.com>
2022-06-09 11:06:18 -0400

    added modules and started the auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-// Import the module for the database
+// Import custom modules
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { SpellModule } from './spell/spell.module';
-
-// Import custom modules
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [AuthModule, ProductsModule, TypeOrmModule.forRoot(), UserModule, SpellModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot(),
+        ProductsModule,
+        AuthModule,
+        UserModule,
+        SpellModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port: number = 3000;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    await app.listen(port);
+    console.log(`Application is running on: http://127.0.0.1:${[port]}`);
 }
 bootstrap();
